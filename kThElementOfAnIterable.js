5c1816f5366e91fbb3e3383627d03e1e49baf2ee
Angad Gill <angadpalsingh92@gmail.com>
2016-10-10 19:44:00 +0530

    Standard

--- kThElementOfAnIterable.js
+++ kThElementOfAnIterable.js
@@ -1,6 +1,6 @@
 const elementAt = (iterable, index) => {
   /* Takes an iterable and returns it's k'th  element */
-  if (iterable.length === 0){
+  if (iterable.length === 0) {
     return null
   } else if (index === 0) {
     return iterable[0]
