62c9e271396e24aaefe585117cf72395a1a29160
Angad Gill <angadpalsingh92@gmail.com>
2016-10-10 18:21:50 +0530

    3rd Problem

---
+++ kThElementOfAnIterable.js
@@ -0,0 +1,8 @@
+const elementAt = (iterable, index) => {
+  /* Takes an iterable and returns it's k'th  element */
+  if (iterable.length === 0){
+    return null
+  } else if (index === 0) {
+    return iterable[0]
+  } return elementAt(iterable.slice(1), --index)
+}
